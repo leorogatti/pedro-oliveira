bb1945111a21b313b2a87adb71d3c6334fff8a1f
Leonardo Rogatti <leorrogatti@gmail.com>
2023-05-10 22:51:19 -0300

    pedro oliveira

---
+++ script.js
@@ -0,0 +1,18 @@
+function abrirPagina() {
+  document.body.innerHTML = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Pedro Oliveira</title>
+      <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+      <div id="conteudo">
+        <h1>Pedro Oliveira</h1>
+        <img src="pedro oliveira.png" alt="Imagem" class="rodar-imagem">
+        <audio src="yeat.mp3" autoplay loop></audio>
+      </div>
+    </body>
+    </html>
+  `;
+}
